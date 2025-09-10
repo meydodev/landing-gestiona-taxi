2b9925fd9b5257ffe9c7a52978b035033b72d490
meydodev <meydodev@gmail.com>
2025-09-10 19:06:04 +0200

    firsrt commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // opcional, por si usas modo oscuro por clase
+  content: [
+    "./src/**/*.{html,ts}",    // plantillas y templates inline
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
